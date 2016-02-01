4e1f1eabe9848049614e5f12ec74342137f020f6
Lindsey Roman <lindsey.roman.geekwise@gmail.com>
2016-02-01 21:15:03 +0000

    finished status

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
 
 app.use(express.static(__dirname + '/static'));
 app.listen(process.env.PORT);
 
-  mongodb://linds-roman:lindsey188@ds060478.mongolab.com:60478/upgrade
+ mongoose.connect('mongodb://linds-roman:lindsey188@ds060478.mongolab.com:60478/upgrade') 
