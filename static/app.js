4225eb5cffd557da81260ec4bc63583986404aec
geekwise_davis_hughes <geekwise.davis.hughes@gmail.com>
2016-02-02 21:20:52 +0000

    commited

--- static/app.js
+++ static/app.js
@@ -7,4 +7,37 @@ angular
           url: "/gradebook",
           templateUrl: "gradebook.html"
         })
+  .state('admin', {
+          url: "/admmin",
+          templateUrl: "admin.html"
+        })
+  .state('classes', {
+          url: "/classes",
+          templateUrl: "classes.html"
+        })
+  .state('index', {
+          url: "/index",
+          templateUrl: "index.html"
+        })
+        .state('register', {
+          url: "/register",
+          templateUrl: "register.html"
+        })
+  .state('teachers', {
+          url: "/teachers",
+          templateUrl: "teachers.html"
+        })
+  
+  .state('transcripts', {
+          url: "/transcripts",
+          templateUrl: "transcripts.html"
+        });
+  
+  
+  
+  
+  
+  
+  
     });
+    
