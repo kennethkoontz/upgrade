68234e91b3020fc4d16f8d05138a8d84260ea705
kenneth koontz <ken@dwibbles.com>
2016-02-03 23:35:58 +0000

    controller stuff

--- static/gradebook.controller.js
+++ static/gradebook.controller.js
@@ -1,5 +1,8 @@
 angular
     .module('app')
     .controller('GradebookController', function($scope) {
-        
+        $scope.classes = [
+            { period: 1, name: 'World History', class_id: 'HO5', grade: 'A+' }
+        ];
+            
     });
