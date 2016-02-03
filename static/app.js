55455319fe18be57b569d83e46508d6d061b9b36
Lindsey Roman <lindsey.roman.geekwise@gmail.com>
2016-02-03 22:50:15 +0000

    added controllers

--- static/app.js
+++ static/app.js
@@ -11,23 +11,28 @@ angular
   .state('login', {
           url: "/login",
           templateUrl: "login.html"
+          controller: 'LoginController'
         })
   .state('classes', {
           url: "/classes",
           templateUrl: "classes.html"
+          controller: 'ClassesController'
         })
         .state('register', {
           url: "/register",
           templateUrl: "register.html"
+          controller: 'RegisterController'
         })
   .state('teachers', {
           url: "/teachers",
           templateUrl: "teachers.html"
+          controller: 'TeachersController'
         })
   
   .state('transcripts', {
           url: "/transcripts",
           templateUrl: "transcripts.html"
+          controller: 'TranscriptsController'
         })
         
         
@@ -35,64 +40,77 @@ angular
         .state('worldhistory', {
           url: "/worldhistory",
           templateUrl: "class/worldhistory.html"
+          controller: 'WhController'
         })
         
         .state('art1', {
           url: "/art1",
           templateUrl: "class/art1.html"
+          controller: 'A1Controller'
         })
         
         .state('biology', {
           url: "/biology",
           templateUrl: "class/biology.html"
+          controller: 'BiologyController'
         })
         
         .state('computer', {
           url: "/computer",
           templateUrl: "class/computer.html"
+          controller: 'ComputerController'
         })
         
         .state('dp1', {
           url: "/dp1",
           templateUrl: "class/dp1.html"
+          controller: 'Dp1Controller'
         })
         
         .state('dp23', {
           url: "/dp23",
           templateUrl: "class/dp23.html"
+          controller: 'Dp23Controller'
         })
         .state('english2', {
           url: "/english2",
           templateUrl: "class/english2.html"
+          controller: 'English2Controller'
         })
         .state('english4', {
           url: "/english4",
           templateUrl: "class/english4.html"
+          controller: 'English4Controller'
         })
         
         .state('hjava', {
           url: "/hjava",
           templateUrl: "class/hjava.html"
+          controller: 'HjavaController'
         })
         
         .state('hmath2', {
           url: "/hmath2",
           templateUrl: "class/hmath2.html"
+          controller: 'Hmath2Controller'
         })
         
         .state('math2', {
           url: "/math2",
           templateUrl: "class/math2.html"
+          controller: 'Math2Controller'
         })
   
         .state('matha', {
           url: "/matha",
           templateUrl: "class/matha.html"
+          controller: 'MathaController'
         })
         
         .state('ushistory', {
           url: "/ushistory",
           templateUrl: "class/ushistory.html"
+          controller: 'UshController'
         })
   
   
---
+++ static/computer.controller.js
@@ -0,0 +1,5 @@
+angular
+    .module('app')
+    .controller('GradebookController', function($scope) {
+        
+    });
