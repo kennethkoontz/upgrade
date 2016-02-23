323c3c5d70781f03bac9f3f03b3223f25885be20
geekwise_cobi_maness <geekwise.cobi.maness@gmail.com>
2016-02-23 21:51:11 +0000

    update classes

--- static/a1.controller.js
+++ static/a1.controller.js
@@ -1,10 +1,11 @@
 angular
     .module('app')
-    .controller('A1Controller', function($scope) {
-        $scope.forms = [
+    .controller('A1Controller', function(){
+        var vm=this;
+        vm.forms = [
             { _id: 'A01', teacher: 'Kate Waldner', description: 'lorem ipsum', students: '21'}    
         ];
-        $scope.assignments = [
+        vm.assignments = [
             { name: 'Sketchbook entry #3', number: 'A001', date: '5/9/16'},
             { name: 'Sketchbook entry #4', number: 'A002', date: '5/11/16'},
             { name: 'Pop Art Draft', number: 'A003', date: '5/13/16'}
